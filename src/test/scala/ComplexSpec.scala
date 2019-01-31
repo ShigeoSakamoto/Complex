8706939ba10bd409d2878eebb6a956618d78af7d
Shigeo Sakamoto <ssakamot@yahoo-corp.jp>
2019-01-31 17:25:29 +0900

    test commit

--- src/test/scala/ComplexSpec.scala
+++ src/test/scala/ComplexSpec.scala
@@ -5,7 +5,7 @@ class ComplexSpec extends FlatSpec with DiagrammedAssertions {
   val c1 = new Complex(1,1)
 
   assert(c1.toString() === "1.0+1.0i")
-  assert((new Complex(-1, -1)).toString() === "-1.0-1.0i")
+  assert(new Complex(-1, -1).toString() === "-1.0-1.0i")
 
   assert(c1.abs() === Math.sqrt(2))
 
@@ -18,21 +18,21 @@ class ComplexSpec extends FlatSpec with DiagrammedAssertions {
   val c4 = new Complex(0,1)
   assert(c4.multiplication(c4).equal(new Complex(-1, 0)))
 
-  assert((new Complex(1,0)).multiplication(new Complex(1,0)).equal(new Complex(1,0)))
+  assert(new Complex(1,0).multiplication(new Complex(1,0)).equal(new Complex(1,0)))
 
-  assert((new Complex(1,1)).conjugate().equal(new Complex(1, -1)))
+  assert(new Complex(1,1).conjugate().equal(new Complex(1, -1)))
 
   c2.multiplication(c3).conjugate().equal(c2.conjugate().multiplication(c3.conjugate()))
   c2.conjugate().conjugate().equal(c2)
   assert(c2.abs() === c2.conjugate().abs())
 
-  assert((new Complex(2,3)).re() === 2)
-  assert((new Complex(2,3)).im() === 3)
+  assert(new Complex(2,3).re() === 2)
+  assert(new Complex(2,3).im() === 3)
 
   assert(c2.add(c2.conjugate()).re() === c2.re() * 2)
   assert(c2.sub(c2.conjugate()).im() === c2.im() * 2)
 
   c1.pow(2).equal(c1.multiplication(c1))
-  c1.pow(2).equal(c1.multiplication(c1))
+  c2.pow(2).equal(c2.multiplication(c2))
 
 }
